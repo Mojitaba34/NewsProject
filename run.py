9ee98bea6234120cfb7d3d5972970602c7fddaf2
efipy <erfan.mosaddeghi@gmail.com>
2020-06-10 11:12:57 +0430

    gtag in base.html for google analytics

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import app
 from app.robots import robot_runner
 if __name__ == "__main__":
-    robots = robot_runner.robot_runner()  
-    robots.threadRun()
+    #robots = robot_runner.robot_runner()  
+    #robots.threadRun()
     app.run("0.0.0.0", 5000, debug=True)
