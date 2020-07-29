053c88ce537f6778cea6c65172831c99a1f4ddaa
efi.py <erfan.mosaddeghi@gmail.com>
2020-07-29 14:49:32 +0430

    config some changee

--- app/admin/config.py
+++ app/admin/config.py
@@ -1,7 +1,7 @@
 # MYSQL.CONNECTOR Config
-MYSQL_HOST = "newshost"
-MYSQL_USERNAME = "root" 
-MYSQL_PASSWORD = "efiAmirMoji@123"
+MYSQL_HOST = "localhost"
+MYSQL_USERNAME = "admin" 
+MYSQL_PASSWORD = "1234"
 MYSQL_PORT = 3306
 MYSQL_DB_NAME = "newsdb"
 
